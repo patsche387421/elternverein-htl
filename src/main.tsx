ca5dbd8ecb7076014f2d0c549342aa809c8f8a7d
Patrick Kroeger <patrick.l.kroeger@gmail.com>
2026-02-20 09:40:31 +0100

    Analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.tsx'
 import './i18n';
@@ -8,5 +10,7 @@ import './i18n';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
